35752987e1df3110190dd363e5c94fe5e0522eae
Naren Kumar Konchada <nkonchad@asu.edu>
2018-10-24 23:04:50 -0700

    US#45 Task#57 Server files, encrypt functionality

--- front-end/app/routes.js
+++ front-end/app/routes.js
@@ -1,18 +1,15 @@
 angular.module('airlineApp', ['ngRoute'])
-.config(function($routeProvider)
+.config(function($routeProvider, $locationProvider)
 {
     $routeProvider
     .when('/home',{
         templateUrl: 'app/views/index.html'
     });
 
-   
-    // $routeProvider .when('/login', {
-    //     templateUrl: 'app/views/pages/login.html'
-    // });
-
-    // $routeProvider .when('/about',{
-    //     templateUrl: 'app/views/pages/about.html'
-    // });
+    $locationProvider.html5Mode({
+        enabled : true,
+        requireBase : false
+    });
+    
 });
 
--- server.js
+++ server.js
@@ -2,11 +2,18 @@ var express= require('express');
 var app= express();
 var port = process.env.PORT || 8080;
 var morgan = require('morgan');
-var path= require('path');
+var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose= require('mongoose');
+var router = express.Router();
+var appRoutes = require('./app/routes/api')(router);
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static(__dirname+ '/front-end'));
-var mongoose= require('mongoose');
+app.use('/api',appRoutes);
+
 mongoose.connect('mongodb://localhost:27017/SER_515', function(err){
     if(err){
         console.log('Not connected');
@@ -15,6 +22,10 @@ mongoose.connect('mongodb://localhost:27017/SER_515', function(err){
         console.log("Connected to MONGODB");
     }
 });
+
+
+
+
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname+ '/front-end/app/views/index.html'));
 })
