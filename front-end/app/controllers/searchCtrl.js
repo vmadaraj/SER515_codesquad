4782b02051d0f2a820bfeb85792b49034292b8b5
Jahnavi Bantupalli <jbantupa@asu.edu>
2018-10-26 16:45:04 -0700

    #58 getting the details entered in form

--- front-end/app/controllers/searchCtrl.js
+++ front-end/app/controllers/searchCtrl.js
@@ -7,7 +7,18 @@ angular.module('flightController',[]).config(function(){
 
 .controller("searchController", function($scope, $location) {
     console.log('testing s');
-   
+    $scope.submit = function(form){
+        console.log('submitted');
+        console.log($scope.source);
+        var flight = {
+              'source' : $scope.source,
+              'destination' : $scope.destination,
+              'departDate' : $scope.departDate,
+              'returnDate' : $scope.returnDate,
+            }
+     console.log(flight);
+    // $http.post('api/searchFlightOn',this.flight);
+    }
     $scope.goto = function(page) {
         console.log(page);
         $location.path(page);
--- front-end/app/routes.js
+++ front-end/app/routes.js
@@ -1,18 +1,22 @@
 angular.module('appRoutes', ['ngRoute'])
-.config(function($routeProvider)
+.config(function($routeProvider,$locationProvider)
 {
     console.log('testing routes...');
     $routeProvider
     .when('/search',{
         templateUrl: 'app/views/pages/booking.html',
+        
+    })
+    .when('/',{
+        
         controller:'searchController',
         controllerAs:'search'
     })
     .when('/home',{
         templateUrl: 'app/views/index.html'
     });
-
-   
+    
+    
     // $routeProvider .when('/login', {
     //     templateUrl: 'app/views/pages/login.html'
     // });
