bbf71144e21c653f2e7a5da2f3aeaa8f4acde822
Behnaz Sabbaghi <behnazsabbaghi@gmail.com>
2018-10-28 02:29:08 -0700

    Added api/authenticate routes

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,26 +1,25 @@
-var User = require('../models/user');
+	router.post('/authenticate', function(req,res) {
+		User.findOne({username: req.body.username}).select('email username password').exec(function(err,user){
+			if(err) throw err;
 
-module.exports =  function(router){
-        // http://localtest:8080/users
-        router.post('/users', function(req, res) {
-        var user = new User();
-        user.username = req.body.username;
-        user.password = req.body.password;
-        user.email = req.body.email;
-
-        if (user.username == null || user.username == '' || user.password == null || user.password == '' || user.email == null || user.email == '') {
-            res.send('Please provide the username, password and email')
-        }
-        else {
-            user.save(function(err){
-                if (err) {
-                    res.send('UserName or Email already exists');
-                }
-                else {
-                    res.send('user created');
-                }
-            });
-        }
-    });
-    return router;
+			if(!user) {
+				res.json({ success: false, message: 'Could not authenticate user' });
+			} else if (user) {
+				if (req.body.password) {
+					var validPassword = user.comparePassword(req.body.password);
+				} else {
+					res.json({ success: false, message: 'No password provided'});
+				}
+				if (!validPassword) {
+					res.json({ success: false, message: 'Could not authenticate password'});
+				} else {
+					res.json({ success: true, message: 'User authenticated!' });
+				  }
+			}
+		});
+	});
+	return router;
 }
+
+
+
