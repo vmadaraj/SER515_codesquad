d65ccd956f8b236f20debf3bb7e9519228562536
Jahnavi Bantupalli <jbantupa@asu.edu>
2018-10-30 22:03:51 -0700

    #64 Changes to app.js

--- front-end/app/app.js
+++ front-end/app/app.js
@@ -1,34 +1,4 @@
 
 var app = angular.module('airlineApp',['appRoutes', 'flightController']).config(['$locationProvider', function($locationProvider) {
   $locationProvider.hashPrefix('');
-}])
- 
- app.config(function($routeProvider)
-{
-    $routeProvider
-
-    .when('/login', {
-        templateUrl: 'front-end/app/views/pages/login.html'
-    });
-
-    $routeProvider
-
-    .when('/about', {
-        templateUrl: 'front-end/app/views/pages/about.html'
-    });
-
-    $routeProvider
-
-    .when('/userProfile', {
-        templateUrl: 'front-end/app/views/pages/userProfile.html'
-    });
-
-    $routeProvider
-
-    .when('/register', {
-        templateUrl: 'front-end/app/views/pages/register.html',
-        controller: 'regController',
-        controllerAs: 'register'
-
-    });
-});
+}]);
