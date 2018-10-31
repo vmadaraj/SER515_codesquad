c86df3a2149975aff5f03300d7f0e5928455ab31
shirikvs <vkakarl@asu.edu>
2018-10-31 14:38:13 -0700

    Task 47: To display the flights.

--- front-end/app/controllers/flightDetailsCtrl.js
+++ front-end/app/controllers/flightDetailsCtrl.js
@@ -1,4 +1,6 @@
-angular.module('detailsController',[])
+angular.module('detailsController',[]).config(function(){
+    console.log('testing details ctrl');
+})
 
 .controller("flightsDetailsController", ['$scope', '$http' ,function($scope, $http) {
   $http.get("../../resources/JSON/flightDetails.json").success(function(data){
