29dede14f31b75af01ca76b2cfb0787b85359fe5
Naren Kumar Konchada <nkonchad@asu.edu>
2018-10-31 10:07:33 -0700

    US#44 Task#61#63 Login frontend and backend implementation

--- app/routes/api.js
+++ app/routes/api.js
@@ -26,28 +26,34 @@ module.exports =function(router){
         user.email = req.body.email;
 
         if (user.username == null || user.username == '' || user.password == null || user.password == '' || user.email == null || user.email == '') {
-            res.send('Please provide the username, password and email')
+            res.json({success : false, message : 'Please provide the username, password and email'});
         }
         else {
             user.save(function(err){
                 if (err) {
-                    res.send('UserName or Email already exists');
+                    res.json({success : true, message : 'UserName or email already exists!!'});
                 }
                 else {
-                    res.send('user created');
+                    res.json({success : true, message : 'user created'});
                 }
             });
         }
     });
 
     router.post('/authenticate', function(req, res) {
-        user.findOne({username : req.body.username}).select('email username password').exec(function(err, user) {
+        User.findOne({ username : req.body.username}).select('email username password').exec(function(err, user) {
             if (err) throw err;
 
             if (!user) {
                 res.json({success : false, message : 'Could not authenticate user'});
             }
             else if (user) {
+               if (req.body.password){
+                   var validPassword = user.comparePassword(req.body.password);
+               }
+               else {
+                   res.json({success : false, message : "No password Provided"});
+               }
                var validPassword =  user.comparePassword(req.body.password);
                if (!validPassword) {
                    res.json({success : false, message : "Could not authenticate password"});
