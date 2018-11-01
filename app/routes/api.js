6e636536e81130bea66ce1a768b96435c249e832
Jahnavi Bantupalli <jbantupa@asu.edu>
2018-10-31 22:13:59 -0700

    Merging master

--- app/routes/api.js
+++ app/routes/api.js
@@ -60,6 +60,31 @@ router.post('/authenticateFlights',function(req,res){
         }
     });
 })
+
+router.post('/authenticate', function(req, res) {
+    User.findOne({ username : req.body.username}).select('email username password').exec(function(err, user) {
+        if (err) throw err;
+
+        if (!user) {
+            res.json({success : false, message : 'Could not authenticate user'});
+        }
+        else if (user) {
+           if (req.body.password){
+               var validPassword = user.comparePassword(req.body.password);
+           }
+           else {
+               res.json({success : false, message : "No password Provided"});
+           }
+           var validPassword =  user.comparePassword(req.body.password);
+           if (!validPassword) {
+               res.json({success : false, message : "Could not authenticate password"});
+           }
+           else {
+               res.json({success : true, message : 'User authenticate'});
+           }
+        }
+    });
+});
 return router;
 }
 
