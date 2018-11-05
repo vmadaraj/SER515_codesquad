8355a3a2067a5db6e9133a326dd4561df9310e38
Naren Kumar Konchada <nkonchad@asu.edu>
2018-11-05 10:06:54 -0700

    changes to the api.js file

--- app/routes/api.js
+++ app/routes/api.js
@@ -5,79 +5,44 @@ var secret = 'tokenTest';
 
 module.exports =function(router){
 
-router.post('/searchFlightOn',function(req,res){
-    var flight = new Flight();
-    flight.flightID = req.body.flightID
-    flight.source = req.body.source;
-    flight.destination = req.body.destination;
-    flight.date =req.body.date;
-    flight.fare = req.body.fare;
-    flight.save(function(err){
-        if(err)
-        res.send(err);
-        else
-        res.send('Flight created');
+    router.post('/searchFlightOn',function(req,res){
+        var flight = new Flight();
+        flight.source = req.body.source;
+        flight.destination = req.body.destination;
+        flight.departDate =req.body.departDate;
+        flight.returnDate = req.body.returnDate;
+        flight.save(function(err){
+            if(err)
+            res.send(err);
+            else
+            res.send('Flight created');
 
 
-    });
-});
-// http://localtest:8080/users
-router.post('/users', function(req, res) {
-    var user = new User();
-    user.username = req.body.username;
-    user.password = req.body.password;
-    user.email = req.body.email;
-
-    if (user.username == null || user.username == '' || user.password == null || user.password == '' || user.email == null || user.email == '') {
-        res.send('Please provide the username, password and email')
-    }
-    else {
-        user.save(function(err){
-            if (err) {
-                res.send('UserName or Email already exists');
-            }
-            else {
-                res.send('user created');
-            }
         });
-    }
-});
-router.post('/authenticateFlights',function(req,res,$http){
-    Flight.find({source:req.body.source, destination:req.body.destination,date:req.body.departDate}).select('flightID source destination date fare').exec(function(err,dflights){
-        if(err) throw err;
-        console.log('flight data')
+    });
+    // http://localtest:8080/users
+    router.post('/users', function(req, res) {
+        var user = new User();
+        user.username = req.body.username;
+        user.password = req.body.password;
+        user.email = req.body.email;
 
-        if(!dflights){
-            res.json({success:false, message: 'Could not find flights'})
+        if (user.username == null || user.username == '' || user.password == null || user.password == '' || user.email == null || user.email == '') {
+            res.json({success : false, message : 'Please provide the username, password and email'});
         }
-        else{
-                console.log(dflights)
-                res.json(dflights)
-                var fs = require('fs');
-
-                var data = "New File Contents";
-                //console.log(res)
-               // angular.json(flights)
-                fs.writeFile('front-end/resources/JSON/departFlights.JSON',JSON.stringify (dflights), function(err, data){
-                if (err) console.log(err);
-               // console.log("Successfully Written to File.");
-});
-
-
-        }
-        Flight.find({source:req.body.destination, destination:req.body.source,date:req.body.returnDate}).select('flightID source destination date fare').exec(function(err,rflights){
-            console.log("hey")
-            res.json(rflights)
-
-            fs.writeFile('front-end/resources/JSON/returnFlights.JSON',JSON.stringify (rflights), function(err, data){
-                if (err) console.log(err);
-               // console.log("Successfully Written to File.");
-});
+        else {
+            user.save(function(err){
+                if (err) {
+                    res.json({success : true, message : 'UserName or email already exists!!'});
+                }
+                else {
+                    res.json({success : true, message : 'user created'});
+                }
             });
+        }
     });
 
     router.post('/authenticate', function(req, res) {
-      console.log("came here after hit");
         if (req.body.username == null || req.body.username == '' || req.body.password  == null || req.body.password == '') {
             res.json({success : false, message : 'Please provide Valid username and password'});
         }
@@ -127,12 +92,6 @@ router.post('/authenticateFlights',function(req,res,$http){
 
     router.post('/me', function(req, res){
         res.send(req.decoded);
-
     });
-});
-
-router.post('/authenticateFlightReturn', function(req, res) {
-    console.log(req.body)
-});
-return router;
+    return router;
 }
